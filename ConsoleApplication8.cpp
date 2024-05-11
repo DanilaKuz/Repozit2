2cf9938a80c18253d059f05c74f99296d8fd1ce2
DanilaKuz <dankuz068@gmail.com>
2024-05-11 22:40:16 +0300

    Update ConsoleApplication8.cpp

--- ConsoleApplication8.cpp
+++ ConsoleApplication8.cpp
@@ -7,15 +7,6 @@ using namespace std;
 
 int main()
 {
-    /*int n;
-    cin >> n;
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < n; j++) {
-            cout << "*";
-        }
-        cout << endl;
-    }*/
-
     int n;
     cin >> n;
     for (int i = 0; i < n; i++) {
